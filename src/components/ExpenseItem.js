a7243c0a77e1a0c896017a7b88236facc3455869
fring.codesmith <banedictfring.codesmith@gmail.com>
2019-11-25 14:10:02 +0600

    component setup

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            hello form expense Item
+        </div>
+    );
+}
+
+export default ExpenseItem;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseList = () => {
+    return (
+        <div>
+            expense List
+            <ExpenseItem />
+        </div>
+    );
+}
+
+export default ExpenseList;
+
